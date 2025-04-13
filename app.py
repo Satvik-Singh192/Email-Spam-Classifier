4175d450efe3f196797e6d852d869ab9b282f142
Satvik-Singh192 <satviksingh192@gmail.com>
2025-04-13 17:12:18 +0530

    added the input box

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import pickle 
 from sklearn.feature_extraction.text import TfidfVectorizer
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import re
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
@@ -12,21 +12,32 @@ nltk.download('stopwords')
 model = tf.keras.models.load_model('spam_classifier_model.keras')
 with open('vectorizer.pkl', 'rb') as f:
     vectorizer = pickle.load(f)
+
 app = Flask(__name__)
 CORS(app) 
 
 stemmer = PorterStemmer()
 stop_words = set(stopwords.words('english'))
 
-@app.route('/', methods=['GET'])
-def hello():
-    return jsonify({"message": "Hello from Flask!"})
-
 def preprocess_message(mssg):
-    mssg=mssg.lower()
+    mssg = mssg.lower()
     mssg = re.sub(r'[^\w\s]', '', mssg)    
     mssg = ' '.join([stemmer.stem(word) for word in mssg.split() if word not in stop_words])
     return mssg
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        sentence = request.form['sentence']
+        preprocessed_sentence = preprocess_message(sentence)
+        sentence_tfidf = vectorizer.transform([preprocessed_sentence]).toarray()
+        prediction = model.predict(sentence_tfidf)
+        predicted_class = "Spam" if prediction[0] >= 0.5 else "Not Spam"
+        return render_template('index.html', 
+                               sentence=sentence, 
+                               prediction=predicted_class, 
+                               probability=round(float(prediction[0][0]), 4))
+    return render_template('index.html', sentence=None)
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -39,7 +50,7 @@ def predict():
     return jsonify({
         'sentence': sentence,
         'prediction': predicted_class,
-        'probability': (float(prediction[0][0]))
+        'probability': float(prediction[0][0])
     })
 
 if __name__ == '__main__':
